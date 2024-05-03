842f57075aa6edd3cd03eda045d3aa0c7fffdd15
Gabriel Colle <gabrielcolle3@hotmail.com>
2024-05-03 19:21:04 -0300

    feat: add initial header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import Header from "./_components/Header";
+
 const Home = () => {
-  return <div className="mx-6 bg-red-500 py-5"></div>;
+  return (
+    <div className="mx-6 bg-red-500 py-5">
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
