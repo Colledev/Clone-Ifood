0e820afbd89efc9596d13e9798994a9b41ab1bb8
Gabriel Colle <gabrielcolle3@hotmail.com>
2024-05-09 01:29:56 -0300

    feat: add restaurant list components

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Search from "./_components/search";
 import { Button } from "./_components/ui/button";
 import { db } from "./_lib/prisma";
 import PromoBanner from "./_components/promoBanner";
+import RestaurantList from "./_components/restaurantList";
 
 const Home = async () => {
   const products = await db.product.findMany({
@@ -24,7 +25,7 @@ const Home = async () => {
     },
   });
   return (
-    <div>
+    <>
       <Header />
       <div className="px-5 pt-6">
         <Search />
@@ -57,7 +58,21 @@ const Home = async () => {
           alt="A partir de R$17,90 em lanches"
         />
       </div>
-    </div>
+
+      <div className="space-y-4 py-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Restaurantes Recomendados</h2>
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+          >
+            Ver todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+        <RestaurantList />
+      </div>
+    </>
   );
 };
 
